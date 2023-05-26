3c1193dd0a427e23bb924583a5b9450f90eeb1bf
Garrett Brown <gort@apx_managed_dnf.vanilla>
2023-05-26 15:32:01 -0500

    Rewrote things so websockets worked

--- client/script.js
+++ client/script.js
@@ -1,36 +1,25 @@
-const ws = new WebSocket("ws://localhost:8080");
+var ws = new WebSocket("ws://localhost:8080");
+
+ws.addEventListener("open", (event) => {
+    ws.send("init");
+});
+
+ws.addEventListener("message", (event) =>{
+    if(event.data == "Connected"){
+        console.log("You're Connected!");
+    }
+    else if(event.data == "TicketError"){
+        alert("Failed to submit ticket. Try again later.");
+    }
+    else if(event.data == "TicketAdded"){
+        alert("Ticket submitted successfully!");
+    }
+});
 
 function CreateTicket(){
-    const title = document.getElementById("titleBox").value;
-    const description = document.getElementById("descriptionBox").value;
-    const email = document.getElementById("emailBox").value;
-    var newTicket = `{
-    "title": "${title}", 
-    "description": "${description}", 
-    "email": "${email}", 
-    "status": "ToDo"
-}`;
-    SubmitTicket(newTicket);
-}
-
-function SubmitTicket(ticket){
-    ws.addEventListener("open", (event) => {
-        ws.send("init");
-    });
-    ws.addEventListener("message", (event) => {
-        const msg = `${event.data}`;
-        if(msg.includes("LoggedIn")){
-
-        }
-        else if(msg == "TicketError"){
-            alert("There was a problem sending your ticket. Try again. If this issue persists, contact IT through email and let them know of your issue.");
-        }
-        else if(msg == "TicketAdded"){
-            alert("Your ticket was successfully submitted! IT will respond to you as soon as possible!");
-        }
-        else if(msg == "Connected"){
-            console.log("Connected to server!");
-            console.log("If you're reading this, the governor of Misouri thinks you're a hacker :P");
-        }
-    });
+    var title = document.getElementById("titleBox").value;
+    var description = document.getElementById("descriptionBox").value;
+    var email = document.getElementById("emailBox").value;
+    var newTicket = {title: title, description: description, email: email, status: "ToDo"}
+    ws.send("Ticket~" + JSON.stringify(newTicket));
 }
--- server/index.js
+++ server/index.js
@@ -1,50 +1,48 @@
-const WebSocket = require("ws");
-const filesystem = require("fs");
+const ws = require('ws');
+const fs = require('fs');
+const server = new ws.Server({"port": "8080"});
+const database = fs.readFileSync("database.txt");
+if(database == ""){
+    var tickets = [];
+}
+else{
+    tickets = JSON.parse(database);
+}
 
-const server = new WebSocket.Server({ port: 8080 });
-const fileData = filesystem.readFileSync("database.txt", "utf8");
-const tickets = JSON.parse(fileData);
-console.log("Server Started! :)");
-server.on("connect", ws =>{
-    ws.on("message", data => {
+server.on("connection", socket => {
+    socket.send("Connected");
+    socket.on("message", data =>{
         var msg = `${data}`;
-        if(msg.includes("ITLogin")){
-            console.log("IT Staff Logged In.");
-            var ticketsJson = JSON.stringify(tickets);
-            ws.send("LoggedIn~" + ticketsJson);
+        if(msg == "init"){
+            console.log("Client Connected")
         }
-        // else if(msg == "EmployeeLogin"){
-        //     console.log("Staff Member Logged In");
-        // }
-        //Ticket format: {"title": "Ticket Issue", "description": "Description of the issue.", "email": "person@email.com", "status": "Ticket Status"}
+        else if(msg.includes("Login")){ //Just for IT page
+            socket.send("Database~" + JSON.stringify(tickets));
+            console.log("IT Logged In.");
+        }
         else if(msg.includes("Ticket")){
             var msgSplit = msg.split("~");
-            var ticket = Json.parse(msgSplit[1]);
-            var prevLength = tickets.length;
-            tickets.push(ticket);
-            if(tickets.length == prevLength){
-                console.log("Ticket wasn't added.");
-                ws.send("TicketError");
+            var newTicketJson = msgSplit[1];
+            var newTicket = JSON.parse(newTicketJson);
+            var oldLength = tickets.length;
+            tickets[oldLength] = newTicket;
+            if(tickets.length == oldLength){
+                socket.send("TicketError");
+                console.log("Ticket Add Failed");
             }
             else{
-                console.log("Ticket added successfully.");
-                ws.send("TicketAdded"); 
+                socket.send("TicketAdded");
+                console.log("Ticket successfully Added");
             }
         }
-        //UpdateTicket format: UpdateTicket~index~NewStatus
         else if(msg.includes("UpdateTicket")){
             var msgSplit = msg.split("~");
-            tickets[msgSplit[1]].status = msgSplit[2];
-            console.log("Status changed to: " + tickets[msgSplit[1]].status);
-        }
-        else if(msg == "init"){
-            console.log("Client Connected");
-            ws.send("Connected");
+            var index = msgSplit[1];
+            tickets[index].status = msgSplit[2];
         }
     });
-
-    ws.on("close", () => {
-        filesystem.writeFileSync("database.txt", JSON.stringify(tickets));
-        console.log("Client Disconnected.");
+    socket.on("close", ()=>{
+        fs.writeFileSync("database.txt", JSON.stringify(tickets));
+        console.log("Client Disconnected");
     });
 });
