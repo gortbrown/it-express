28233b6591d1bc7caf3c5d6e7c80c085ceb53581
Garrett <gjbrown@silverblue>
2023-05-27 17:23:19 -0400

    Started work on the IT interface

--- client/script.js
+++ client/script.js
@@ -1,4 +1,5 @@
 var ws = new WebSocket("ws://localhost:8080");
+var tickets = [];
 
 ws.addEventListener("open", (event) => {
     ws.send("init");
@@ -14,6 +15,11 @@ ws.addEventListener("message", (event) =>{
     else if(event.data == "TicketAdded"){
         alert("Ticket submitted successfully!");
     }
+    else if(event.data.includes("Database")){
+        msgSplit = event.data.split("~");
+        tickets = JSON.parse(msgSplit[1]);
+        LoadTickets();
+    }
 });
 
 function CreateTicket(){
@@ -22,4 +28,21 @@ function CreateTicket(){
     var email = document.getElementById("emailBox").value;
     var newTicket = {title: title, description: description, email: email, status: "ToDo"}
     ws.send("Ticket~" + JSON.stringify(newTicket));
+}
+
+function HandleLogin(){
+    ws.send("Login");
+}
+
+function LoadTickets(){
+    var ticketViewHtml = "";
+    tickets.forEach(t => {
+        var ticketHtml = `<h2>${t.title}</h2>
+        <h4>From: ${t.title}</h4>
+        <h4>Status: ${t.status}</h4>
+        <p>${t.description}</p>`;
+
+        ticketViewHtml.concat(ticketHtml);
+    });
+    document.getElementById("ticketview").innerHTML = ticketViewHtml;
 }
